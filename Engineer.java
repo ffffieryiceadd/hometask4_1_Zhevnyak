0554e770e082e39a7214f436f6b1d1f043a6c1a2
Oleg Zhevnyak <ffieryice@gmail.com>
2020-04-11 20:00:02 +0300

    Add

---
+++ Engineer.java
@@ -0,0 +1,29 @@
+package worker;
+
+import entity.Result;
+import entity.Test;
+
+public abstract class Engineer {
+    private int skill = (int) (Math.random() * 10 + 1);
+    private int anxiety = 3;
+
+    public int getSkill() {
+        return skill;
+    }
+
+    public void setSkill(int skill) {
+        this.skill = skill;
+    }
+
+    public int getAnxiety() {
+        return anxiety;
+    }
+
+    public void setAnxiety(int anxiety) {
+        this.anxiety = anxiety;
+    }
+
+    public Result executeTest(Test test) {
+        return null;
+    }
+}
