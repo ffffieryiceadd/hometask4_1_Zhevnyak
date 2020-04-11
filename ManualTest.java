fa46a75ebc9a342bdf91953746e9613bdbed9c05
Oleg Zhevnyak <ffieryice@gmail.com>
2020-04-11 19:44:30 +0300

    Add

---
+++ ManualTest.java
@@ -0,0 +1,8 @@
+package entity;
+
+public class ManualTest extends Test {
+    public ManualTest(TestLevel testLevel, int instability) {
+        super(testLevel);
+        this.setInstability(instability);
+    }
+}
