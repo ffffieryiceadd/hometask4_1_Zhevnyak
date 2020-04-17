2dd74c29d58c45b5699998853959de18f4b53a0c
Oleg Zhevnyak <ffieryice@gmail.com>
2020-04-17 21:16:59 +0300

    Changes added in AutomatedTest.java

--- AutomatedTest.java
+++ AutomatedTest.java
@@ -2,7 +2,6 @@ package entity;
 
 public class AutomatedTest extends Test {
     public AutomatedTest(TestLevel testLevel, int instability) {
-        super(testLevel);
-        this.setInstability(instability);
+        super(testLevel, instability);
     }
 }
